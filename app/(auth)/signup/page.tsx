51fa6ce514f785702cd9efc3a72c988f7f8fdf4f
sukhdeep <sukhdeepkumar918@gmail.com>
2026-08-01 16:07:11 +0530

    Black to white

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,4 +1,4 @@
-import SignupForm from "./signup-form";
+﻿import SignupForm from "./signup-form";
 
 // Any server-side data you need
 async function getSignupPageData() {
@@ -15,7 +15,7 @@ export default async function SignupPage() {
   const data = await getSignupPageData();
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-black px-4">
+    <div className="min-h-screen flex items-center justify-center bg-white px-4">
       <SignupForm
         defaultRole={data.defaultRole}
         companyNameHint={data.companyNameHint}
