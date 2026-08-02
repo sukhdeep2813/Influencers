ba4c0f549452edb714031d45747ad01017b5fe1d
sukhdeep <sukhdeepkumar918@gmail.com>
2026-08-02 15:02:38 +0530

    UI Update

--- components/marketing/BackgroundGlows.tsx
+++ components/marketing/BackgroundGlows.tsx
@@ -1,10 +1,10 @@
 export function BackgroundGlows() {
   return (
     <>
-      <div className="absolute top-[-10%] left-[-10%] w-[500px] h-[500px] bg-orange-100/6 rounded-full blur-[120px] pointer-events-none" />
-      <div className="absolute top-[20%] right-[-10%] w-[600px] h-[600px] bg-sky-100/6 rounded-full blur-[150px] pointer-events-none" />
-      <div className="absolute top-[-10%] right-[-10%] w-[550px] h-[550px] bg-amber-100/6 rounded-full blur-[130px] pointer-events-none" />
-      <div className="absolute top-[40%] left-[-15%] w-[650px] h-[650px] bg-sky-100/6 rounded-full blur-[160px] pointer-events-none" />
+      <div className="absolute top-[-10%] left-[-10%] w-125 h-125 bg-orange-100/6 rounded-full blur-[120px] pointer-events-none" />
+      <div className="absolute top-[20%] right-[-10%] w-150 h-150 bg-sky-100/6 rounded-full blur-[150px] pointer-events-none" />
+      <div className="absolute top-[-10%] right-[-10%] w-137.5 h-137.5 bg-amber-100/6 rounded-full blur-[130px] pointer-events-none" />
+      <div className="absolute top-[40%] left-[-15%] w-162.5 h-162.5 bg-sky-100/6 rounded-full blur-[160px] pointer-events-none" />
     </>
   );
 }
