80bdf4570402aa238129b94c954bb58216fc84b5
sukhdeep <sukhdeepkumar918@gmail.com>
2026-08-03 14:35:28 +0530

    Making For Role

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,8 +1,9 @@
 import { createAuthClient } from "better-auth/react";
+// import type { auth } from "./auth";
 
 export const authClient = createAuthClient({
-    // Make sure NEXT_PUBLIC_APP_URL is set in your .env (e.g., http://localhost:3000)
-    baseURL: process.env.NEXT_PUBLIC_APP_URL, 
+  // Make sure BETTER_AUTH_URL is set in your .env (e.g., http://localhost:3000)
+  baseURL: process.env.BETTER_AUTH_URL,
 });
 
 export const { signIn, signUp, useSession } = authClient;
