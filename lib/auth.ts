538ab136a1a78a2da5a0714846d3826a22640f62
sukhdeep <sukhdeepkumar918@gmail.com>
2026-08-03 14:36:23 +0530

    Additional Field for Role

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,12 +3,18 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./prisma"; // Ensure this imports your global Prisma client
 
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql", 
-    }),
-    emailAndPassword: {
-        enabled: true,
-        // Optional: Require email verification before login
-        // requireEmailVerification: true 
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+    // Optional: Require email verification before login
+    // requireEmailVerification: true
+  },
+
+  user: {
+    additionalFields: {
+      role: { type: "string", required: true, defaultValue: "CREATOR" },
     },
+  },
 });
