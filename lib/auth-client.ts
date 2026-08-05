508609f109b4e4fd2490f5c6c686cef8e9884d90
sukhdeep <sukhdeepkumar918@gmail.com>
2026-08-05 21:31:05 +0530

    Removed addition  role

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,9 +1,9 @@
 import { createAuthClient } from "better-auth/react";
-// import type { auth } from "./auth";
 
 export const authClient = createAuthClient({
   // Make sure BETTER_AUTH_URL is set in your .env (e.g., http://localhost:3000)
   baseURL: process.env.BETTER_AUTH_URL,
+
 });
 
 export const { signIn, signUp, useSession } = authClient;
