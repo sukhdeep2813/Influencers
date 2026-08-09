23917f348468d8223d61c72f783f9b4bbdf7489b
sukhdeep <sukhdeepkumar918@gmail.com>
2026-08-09 14:53:53 +0530

    Adding Plugin for Role

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,9 +1,11 @@
 import { createAuthClient } from "better-auth/react";
+import { auth } from "./auth";
+import { inferAdditionalFields } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
   // Make sure BETTER_AUTH_URL is set in your .env (e.g., http://localhost:3000)
   baseURL: process.env.BETTER_AUTH_URL,
-
+  plugins: [inferAdditionalFields<typeof auth>()],
 });
 
 export const { signIn, signUp, useSession } = authClient;
