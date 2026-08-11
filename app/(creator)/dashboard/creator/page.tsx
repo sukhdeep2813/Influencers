87946eaccd6cee4c5740adfa7ad11712af2a23fa
sukhdeep <sukhdeepkumar918@gmail.com>
2026-08-11 16:21:38 +0530

    Need to Update

--- app/(creator)/dashboard/creator/page.tsx
+++ app/(creator)/dashboard/creator/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 const page = () => {
-  return <div>this is creator</div>;
+  return (
+    <div className="text-black pt-48">this is creator</div>
+  );
 };
 export default page;
