fd1b81acfedb5c5bc30d07e59adae5f13fadb19d
sukhdeep <sukhdeepkumar918@gmail.com>
2026-08-13 14:17:40 +0530

    removed Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/ui/Navbar";
+
 import Footer from "@/components/ui/Footer";
 import { Toaster } from "sonner";
 
@@ -10,7 +10,7 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({  
+const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
@@ -31,8 +31,6 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Navbar />
-
         <main className="flex flex-1 flex-col">
           {" "}
           {children}
