e9e37806b2bd585d63e9b77e58e0b5c2e48dc97b
sukhdeep <sukhdeepkumar918@gmail.com>
2026-08-14 17:56:17 +0530

    Header Pages link

---
+++ app/(creator)/dashboard/creator/data/dashboard-header.ts
@@ -0,0 +1,36 @@
+export const tabs = [
+  {
+    label: "Overview",
+    href: "/dashboard/creator",
+  },
+  {
+    label: "Applications",
+    href: "/dashboard/creator/applications",
+    badge: 3,
+  },
+  {
+    label: "Messages",
+    href: "/dashboard/creator/messages",
+    badge: 5,
+  },
+  {
+    label: "Calendar",
+    href: "/dashboard/creator/calendar",
+  },
+  {
+    label: "Portfolio",
+    href: "/dashboard/creator/portfolio-manager",
+  },
+  {
+    label: "Analytics",
+    href: "/dashboard/creator/analytics",
+  },
+  {
+    label: "Reviews",
+    href: "/dashboard/creator/reviews",
+  },
+  {
+    label: "Payments",
+    href: "/dashboard/creator/payments",
+  },
+];
