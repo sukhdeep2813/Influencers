6dbc3c0000f086ca6312b9a1a34913fad09db792
sukhdeep <sukhdeepkumar918@gmail.com>
2026-08-16 18:26:09 +0530

    Layout For Now Removed Role Based Access

--- app/(brand)/dashboard/brand/layout.tsx
+++ app/(brand)/dashboard/brand/layout.tsx
@@ -1,13 +1,27 @@
-export default function AuthLayout({
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // const session = await auth.api.getSession({
+  //   headers: await headers(),
+  // });
+
+  // if (session) {
+  //   redirect(
+  //     session.user.role === "BRAND"
+  //       ? "/dashboard/brand"
+  //       : "/dashboard/creator",
+  //   );
+  // }
+
   return (
-    <div className="flex flex-1 items-center justify-center px-4 py-12 sm:px-6 lg:px-8 w-full">
-      <div className="w-full max-w-4xl space-y-8 p-10 border border-gray-100 rounded-3xl bg-white shadow-[var(--card-shadow)]">
-        {children}
-      </div>
-    </div>
+     <main className="min-h-screen w-full bg-slate-50">
+      {children}
+    </main>
   );
 }
